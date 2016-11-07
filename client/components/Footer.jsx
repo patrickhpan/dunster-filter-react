a733e5a9d0005042a28b3e87afdd159e11981edc
Patrick Pan <patrickpan@college.harvard.edu>
2016-11-07 15:01:55 -0500

    Fix some style issues

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -6,7 +6,9 @@ import Strings from '../json/strings.json';
 class Footer extends React.Component {
     render() {
         return <div id="Footer">
-            <a className="underline" href="http://patrickpan.com"> Made with 😍  &nbsp; by Patrick Pan. Not affiliated with the official Hillary campaign.</a>
+            <a className="underline" href="http://patrickpan.com"> Made with 😍  by Patrick Pan.</a>
+            <a className="underline" href="http://github.com/patrickhpan/im-with-her">Check out the source here.</a>
+            <div>Not affiliated with the official Hillary campaign.</div>
         </div>            
     }
 }
